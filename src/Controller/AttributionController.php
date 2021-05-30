8aa1de850f6936774e335938d7775b4f298383de
Guillaume HARARI <guillaumeharari@hotmail.com>
2021-05-30 23:03:27 +0200

    handle double attribution

--- src/Controller/AttributionController.php
+++ src/Controller/AttributionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Attribution;
 use App\Form\AttributionType;
+use App\Repository\AttributionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,14 +16,22 @@ class AttributionController extends AbstractController
     /**
      * @Route("/admin/attribution", name="attribution")
      */
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request,
+                          EntityManagerInterface $entityManager,
+                          AttributionRepository $attributionRepository): Response
     {
         $attribution = new Attribution();
         $form = $this->createForm(AttributionType::class, $attribution);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($attribution);
-            $entityManager->flush();
+            $hasProgram = $attributionRepository->findOneBy(['user' => $attribution->getUser(), 'program' => $attribution->getProgram()]);
+            if (!$hasProgram) {
+                $entityManager->persist($attribution);
+                $entityManager->flush();
+                $this->addFlash('green', 'Le programe a bien été attribué');
+            } else {
+                $this->addFlash('red', 'Cet utilisateur a déjà ce programe');
+            }
         }
         return $this->render('attribution/index.html.twig', [
             'form' => $form->createView(),
